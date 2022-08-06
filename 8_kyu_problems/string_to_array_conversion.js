45bb017d65fae9e854724aec05ab173928952cfb
Karan Bhatt <karanbhatt21065@gmail.com>
2022-08-06 23:48:38 +0545

    another new codewar challange

---
+++ 8_kyu_problems/string_to_array_conversion.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+// My Solution
+
+function stringToArray(string){
+
+    // code code code
+    myArray = string.split(" ");
+    return myArray;
+  
+  }
